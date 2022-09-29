5f639e49a3cdd9a33bd53d9719909d2c51d48339
Elgun <elgun.ezmemmedov@mail.ru>
2022-09-29 22:22:37 +0400

    'clear'

--- tourProjet/client/src/utility/index.js
+++
@@ -1,6 +0,0 @@
-export const excerpt = (str, count) => {
-  if (str.length > count) {
-    str = str.substring(0, count) + " ...";
-  }
-  return str;
-};
--- tourProjet/server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { signup, signin, googleSignIn } = require("../controller/userC.js");
-const router = express.Router();
-
-router.post("/signup", signup);
-router.post("/signin", signin);
-router.post("/googleSignIn", googleSignIn);
-
-module.exports = router;
