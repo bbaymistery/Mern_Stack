afce966108bc0cce467a6dadca51db2bb15a507d
Elgun <elgun.ezmemmedov@mail.ru>
2022-10-15 11:30:02 +0400

    'final

---
+++ admin/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,118 @@
+import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import StoreIcon from "@mui/icons-material/Store";
+import InsertChartIcon from "@mui/icons-material/InsertChart";
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import { Link } from "react-router-dom";
+import { DarkModeContext } from "../../context/darkModeContext";
+import { AuthContext } from "../../context/AuthContext";
+import { useContext } from "react";
+import axios from "axios";
+
+const Sidebar = () => {
+    const { dispatch } = useContext(DarkModeContext);
+    const { loading, error, dispatch: authDispatch } = useContext(AuthContext);
+
+    const logoutUser = async () => {
+        try {
+            const res = await axios.get("/auth/logout");
+            if (res.status === 200) authDispatch({ type: "LOGOUT" })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return (
+        <div className="sidebar">
+            <div className="top">
+                <Link to="/" style={{ textDecoration: "none" }}>
+                    <span className="logo">baymisteryAdmin</span>
+                </Link>
+            </div>
+            <hr />
+            <div className="center">
+                <ul>
+                    <p className="title">MAIN</p>
+                    <Link to="/">
+                        <li>
+                            <DashboardIcon className="icon" />
+                            <span>Dashboard</span>
+                        </li>
+                    </Link>
+                    <p className="title">LISTS</p>
+                    <Link to="/users" style={{ textDecoration: "none" }}>
+                        <li >
+                            <PersonOutlineIcon className="icon" />
+                            <span >Users</span>
+                        </li>
+                    </Link>
+                    <Link to="/hotels" style={{ textDecoration: "none" }}>
+                        <li>
+                            <StoreIcon className="icon" />
+                            <span>Hotels</span>
+                        </li>
+                    </Link>
+                    <Link to="/rooms" style={{ textDecoration: "none" }}>
+                        <li>
+                            <CreditCardIcon className="icon" />
+                            <span>Rooms</span>
+                        </li>
+                    </Link>
+                    {/* <li>
+                        <CreditCardIcon className="icon" />
+                        <span>Orders</span>
+                    </li>
+                    <li>
+                        <LocalShippingIcon className="icon" />
+                        <span>Delivery</span>
+                    </li>
+                    <p className="title">USEFUL</p>
+                    <li>
+                        <InsertChartIcon className="icon" />
+                        <span>Stats</span>
+                    </li>
+                    <li>
+                        <NotificationsNoneIcon className="icon" />
+                        <span>Notifications</span>
+                    </li>
+                    <p className="title">SERVICE</p>
+                    <li>
+                        <SettingsSystemDaydreamOutlinedIcon className="icon" />
+                        <span>System Health</span>
+                    </li>
+                    <li>
+                        <PsychologyOutlinedIcon className="icon" />
+                        <span>Logs</span>
+                    </li>
+                    <li>
+                        <SettingsApplicationsIcon className="icon" />
+                        <span>Settings</span>
+                    </li>
+                    <p className="title">USER</p>
+                    <li>
+                        <AccountCircleOutlinedIcon className="icon" />
+                        <span>Profile</span>
+                    </li> */}
+                    <li onClick={logoutUser}>
+                        <ExitToAppIcon className="icon" />
+                        <span>Logout</span>
+                    </li>
+                </ul>
+            </div>
+            <div className="bottom">
+                {/*  */}
+                <div className="colorOption" onClick={() => dispatch({ type: "LIGHT" })}  ></div>
+                <div className="colorOption" onClick={() => dispatch({ type: "DARK" })}  ></div>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
